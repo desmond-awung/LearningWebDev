bad6e1e3e0018b585d9e2302b0a747c8e18e0837
Desmond Awungayi <dawungayi@gmail.com>
2020-09-05 21:06:37 -0500

    RESTful Blog App: Added the NEW and CREATE routes, plus included Sematic UI for styling.     - Added the NEW and CREATE routes in app.js, for displaying the form to input info for a new blog post and storing the blog post to DB, respectively.     - Added views/new.ejs, which displays the form for a user to input info for a blog post.     - Added the header file (including a navbar/menu) and footer file for use across all pages in the app. These are located in the /views/partials/ directory.     - Added Semantic UI to be applied to all pages in the project, for better styling; so easy to follow. CDN located in header file.     - Also added a custom CSS file, located in the /public/stylesheets/ directory.

--- Back_End_Lessons/RESTfulBlogApp/app.js
+++ Back_End_Lessons/RESTfulBlogApp/app.js
@@ -1,5 +1,6 @@
 // entry point for Blog app
 
+// APP CONFIG
 // declare packages used
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -12,7 +13,7 @@ app = express();
 app.set("view engine", "ejs"); // skip all .ejs file extensions in routes
 // tell express to use the public/ directory, which will contain our front-end styling: css, js & bootstrap
 // so all html <link> tags in the head will point to public/...
-app.use(express.static("public"));
+app.use(express.static("public"));  // to serve our custom stylesheets
 app.use(bodyParser.urlencoded({extended: true}));   // init body-parser for html passing info from forms
 
 
@@ -24,18 +25,78 @@ mongoose.connect("mongodb://localhost/restful_blog_app", {
 .then(() => console.log("Connected to the RESTful Blog DB"))
 .catch(error => console.log(error.message));
 
+// MONGOOSE/MODEL CONFIG
 
-/* blog model for MongoDB: 
+/* blog schema for MongoDB: 
 title, image, body, date: all strings
 */
 const blogSchema = mongoose.Schema({
     title : String,
+    // we caould also have a default image string url or local image here: 
+    // {type: String, default: "https://..."}
     image : String,
     body : String,
-    date : Date,
+    // don't just say "Date" ==> user would have to manually enter the date.
+    // this obj below initializes the date to be the currrent date by default
+    created : {type: Date, default: Date.now}, 
+});
+
+const Blog = mongoose.model("Blog", blogSchema);    // compile the schema into a model
+
+// // test blog
+// Blog.create(
+//     {
+//         title : "Test Blog",
+//         image : "https://images.unsplash.com/photo-1543285198-3af15c4592ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+//         body : "This is a test blog post", 
+//     }
+// );
+
+/**
+ * RESTFUL ROUTES
+ */
+
+// LANDING PAGE --> INDEX page
+app.get("/", (req, res) => {
+    res.redirect("/blogs");     // from home page, go to the "/blogs" route
+})
+
+// INDEX ROUTE
+app.get("/blogs", (req, res) => {
+    Blog.find((err, blogs) => {
+        if(err) {
+            console.log("Error");
+            console.log(err);
+        } else {
+            // send all blogs content from DB (in the var "blogs") to be displayed in the info page
+            res.render("index", {blogs : blogs});
+        }
+    })
+});
+
+// NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", (req, res) => {
+    // get the blog info from the form and create a Blog document to the DB
+    // console.log(req.body.blog);     // - for debug
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err) {
+            console.log(err);
+            res.render("new")   // if there was an error in saving to db, then we re-display an empty form
+        } else {
+            console.log(`New blog created\n${newBlog}`);
+            // redirect to the index page to show all blogs
+            res.redirect("/blogs");
+        }
+    });
+
 });
 
 
 app.listen(port, () => {
-    console.log(`Blog started at: ${port}`);
+    console.log(`Blog App started at: ${port}`);
 });
