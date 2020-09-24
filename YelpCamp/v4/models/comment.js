a3c1f87c6e9e7accca534f5b7eeaea89e9aa6de3
Desmond Awungayi <dawungayi@gmail.com>
2020-09-23 23:08:56 -0500

    YelpCamp App: Major restructuring of files for straightforward version control.     - Created a single Yelpcamp directory to contain the code for different versions, to allow easy viewing of changes/edits between commits.     - Created the Archive folder to contain all the code from previous versions: v1 to v7. Note: the code in the Yelpcamp root directory is v8.

--- YelpCamp/v4/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const commentSchema = new mongoose.Schema({
-    author : String,
-    content : String,
-});
-
-// export
-module.exports = mongoose.model("Comment", commentSchema); 
